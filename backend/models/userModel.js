6245ad1f6d2335db3caf979b7c4313cef8d03666
pritamkumarbishwas <pritamkumarbishwas85@gmail.com>
2024-07-18 21:27:03 +0530

    create message schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,9 +3,19 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
     {
-        name: { type: "String", required: true },
-        email: { type: "String", unique: true, required: true },
-        password: { type: "String", required: true },
+        name: {
+            type: "String",
+            required: true
+        },
+        email: {
+            type: "String",
+            unique: true,
+            required: true
+        },
+        password: {
+            type: "String",
+            required: true
+        },
         pic: {
             type: "String",
             required: true,
