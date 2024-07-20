fb6f99b5df32584c83c5e7c04aec326a4e31abd3
pritamkumarbishwas <pritamkumarbishwas85@gmail.com>
2024-07-20 22:43:08 +0530

    replace frontend url for origin

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ const server = app.listen(PORT, () =>
 const io = socketio(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-react-pi.vercel.app",
     methods: ["GET", "POST"],
     credentials: true // if your frontend and backend are on different domains
   }
