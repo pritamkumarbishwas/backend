f674316d4cd8c950c14cff5a852a51cb1815c282
pritamkumarbishwas <pritamkumarbishwas85@gmail.com>
2024-07-21 01:43:08 +0530

    fix bugs

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,14 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
+  // app.get("/", (req, res) => {
+  //   res.send("API is running..");
+  // });
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
 }
 
 // Error Handling middleware
