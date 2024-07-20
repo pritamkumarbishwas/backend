e1a953f65670a88ee50b93809a503005417a6cc3
pritamkumarbishwas <pritamkumarbishwas85@gmail.com>
2024-07-21 01:53:10 +0530

    fix bugs

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
 const colors = require("colors"); // Optionally, for colorful console logs
+const http = require("http");
+const socketIo = require("socket.io");
 
 dotenv.config();
 connectDB();
@@ -30,14 +32,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
-  // app.get("/", (req, res) => {
-  //   res.send("API is running..");
-  // });
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
 }
 
 // Error Handling middleware
@@ -46,19 +43,18 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () =>
-  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
-);
+// Create HTTP server
+const server = http.createServer(app);
 
 // Socket.io Setup
-const io = require("socket.io")(server, {
+const io = socketIo(server, {
   cors: {
     origin: "*", // Adjust as needed for security
   },
 });
 
 io.on("connection", (socket) => {
-  console.log("Connected to socket.io");
+  console.log("Socket.io: User connected");
 
   socket.on("setup", (userData) => {
     socket.join(userData._id);
@@ -67,7 +63,7 @@ io.on("connection", (socket) => {
 
   socket.on("join chat", (room) => {
     socket.join(room);
-    console.log(`User joined room: ${room}`);
+    console.log(`Socket.io: User joined room - ${room}`);
   });
 
   socket.on("typing", (room) => {
@@ -82,7 +78,7 @@ io.on("connection", (socket) => {
     const { chat, sender } = newMessageReceived;
 
     if (!chat.users) {
-      console.log("chat.users not defined");
+      console.log("Socket.io: chat.users not defined");
       return;
     }
 
@@ -94,9 +90,14 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected from socket.io");
+    console.log("Socket.io: User disconnected");
   });
 });
+
+// Start server
+server.listen(PORT, () =>
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
 
 // Handle process termination
 process.on("SIGINT", () => {
